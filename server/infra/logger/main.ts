0bc94aff2225f7ea5d07d958e57a655a0f13a2f3
Henrique Barros <henriquebarrosx@gmail.com>
2025-12-06 16:31:47 -0300

    Adiciona factory para registro de logs

---
+++ server/infra/logger/main.ts
@@ -0,0 +1,39 @@
+import Pino from 'pino';
+import { threadId } from "worker_threads";
+
+export const logger = createLogger();
+
+type Logger = {
+	info(message: string, ...args: unknown[]): void;
+	error(message: string | unknown, ...args: unknown[]): void;
+}
+
+export function createLogger(): Logger {
+
+	const pinoLogger = Pino(
+		{
+			transport: {
+				target: 'pino-pretty',
+				options: {
+					translateTime: 'SYS:dd/mm/yyyy HH:MM:ss',
+					colorize: true,
+				}
+			},
+		},
+	)
+
+	function info(message: string, ...args: unknown[]): void {
+		const extra = args.length ? ` ${args.join(' ')}` : '';
+		pinoLogger.info(`[Thread: ${threadId}] - ${message}${extra}`);
+	}
+
+	function error(message: string | unknown, ...args: unknown[]): void {
+		const extra = args.length ? ` ${args.join(' ')}` : '';
+		pinoLogger.error(`[Thread: ${threadId}] - ${message}${extra}`);
+	}
+
+	return {
+		info,
+		error,
+	}
+}
